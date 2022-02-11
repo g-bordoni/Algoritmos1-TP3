2726d0d9aad31d282b8b7423a0cbffc028f444e6
Gabriel Bordoni <bordoni.gabriel@hotmail.com>
2022-02-11 01:29:37 -0300

    improving

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,34 +3,32 @@
 #include <fstream>
 #include <cmath>
 
-int return_previous_path(int *line, int x_index, int M){
-    int start = (x_index - 1 >= 0)? x_index - 1: 0;
-    int end = (x_index + 1 < M)? x_index + 1: M-1;
+int get_value_of_the_best_path_from_the_previous_line(int *previous_line, int current_position_index, int M){
+    int start = (current_position_index - 1 >= 0)? current_position_index - 1: 0;
+    int end = (current_position_index + 1 < M)? current_position_index + 1: M-1;
 
-    int max = -1;
+    int better_value = -1;
     for(int k=start; k<=end; k++){
-        if (max < line[k])
-            max = line[k];
+        if (better_value < previous_line[k])
+            better_value = previous_line[k];
     }
-    return max;
+    return better_value;
 }
 
-int get_step(int* line, int x_index, int M){
-    int start = (x_index - 1 >= 0)? x_index - 1: 0;
-    int end = (x_index + 1 < M)? x_index + 1: M-1;
+int get_step_line_index_of_the_better_path(int* line, int current_position_index, int M){
+    int start = (current_position_index - 1 >= 0)? current_position_index - 1: 0;
+    int end = (current_position_index + 1 < M)? current_position_index + 1: M-1;
 
-    int max = -1;
-    int index = -1;
+    int better_value = -1;
+    int better_value_index_position = -1;
     for(int k=start; k<=end; k++){
-        if (max < line[k]){
-            max = line[k];
-            index = k;
+        if (better_value < line[k]){
+            better_value = line[k];
+            better_value_index_position = k;
         }
     }
-    return index;
-
+    return better_value_index_position;
 }
-
 
 
 int main (int argc, char* argv[])
@@ -42,42 +40,45 @@ int main (int argc, char* argv[])
     std::cin >> data;
     int M = stoi(data);
 
-    int matrix[N][M];
+    int input_problem_matrix[N][M];
     for(int i = 0; i<N; i++){
         for(int j = 0; j<M; j++){
             std::cin >> data;
-            matrix[i][j] = stoi(data);
+            input_problem_matrix[i][j] = stoi(data);
         }
     }
 
-    int matrix_solution[N][M];
+    int better_path_aggregate_value_matrix[N][M];
     for(int k = 0; k<M; k++)
-        matrix_solution[0][k] = matrix[0][k];
+        better_path_aggregate_value_matrix[0][k] = input_problem_matrix[0][k];
 
     for(int i = 1; i<N; i++){
         for(int j = 0; j<M; j++){
-            matrix_solution[i][j] = matrix[i][j] + return_previous_path(matrix_solution[i-1], j, M);
+            better_path_aggregate_value_matrix[i][j] = input_problem_matrix[i][j] + get_value_of_the_best_path_from_the_previous_line(better_path_aggregate_value_matrix[i-1], j, M);
         }
     }
 
     
-    int index = -1;
-    int maximum = -1;
+    int better_path_end_index = -1;
+    int better_path_value = -1;
     for(int j = 0; j<M; j++){
-        if (maximum < matrix_solution[N-1][j]){
-            maximum = matrix_solution[N-1][j];
-            index = j;
+        if (better_path_value < better_path_aggregate_value_matrix[N-1][j]){
+            better_path_value = better_path_aggregate_value_matrix[N-1][j];
+            better_path_end_index = j;
         }
     }
-    int path[N];
-    path[N-1] = index;
+    int better_path[N];
+    better_path[N-1] = better_path_end_index;
     for(int i = N-2; i>=0; i--){
-        path[i] = get_step(matrix_solution[i], path[i+1], M);
+        better_path[i] = get_step_line_index_of_the_better_path(better_path_aggregate_value_matrix[i], better_path[i+1], M);
     }   
 
-    std::cout << std::endl <<  maximum << std::endl;
+    std::cout << better_path_value << std::endl;
     for(int i = 0; i<N; i++){
-        std::cout << path[i] << " ";
-    }  
+        std::cout << better_path[i];
+        if (i != N-1)
+            std::cout << " ";
+    } 
+
     return 0;
 }
